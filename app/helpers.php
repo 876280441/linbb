56c8630fed449c577dce0bf51f14fd6b15f8f87f
linruxin <876280441@qq.com>
2023-03-07 23:15:28 +0800

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -20,3 +20,15 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+/**
+ * 过滤字符
+ * @param $value '待过滤数据'
+ * @param $length '长度'
+ * @return \Illuminate\Support\Stringable|mixed|__anonymous@6424
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str()->limit($excerpt, $length);
+}
