38cf97bc37b535e6fa01e2697066e3544c3c63e1
linruxin <876280441@qq.com>
2023-03-08 22:20:57 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -18,15 +19,23 @@ class TopicObserver
 
     public function updating(Topic $topic)
     {
-        //
+        //在更新标题时，也需要更新slug
+        if ($topic->getOriginal('title') != $topic->title){
+            dispatch(new TranslateSlug($topic));
+        }
     }
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
-        $topic->excerpt = make_excerpt($topic->body);
     }
 }
