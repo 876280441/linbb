9b4606963109fdabaa257548e0c53c50367d1af3
linruxin <876280441@qq.com>
2023-03-08 21:27:12 +0800

    用户可以删除自己发的话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -55,7 +55,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', __('auth.updateSuccessfully'));
 	}
 
 	public function destroy(Topic $topic)
@@ -63,7 +63,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', __('auth.Deleted successfully'));
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
--- lang/zh_CN/auth.php
+++ lang/zh_CN/auth.php
@@ -15,4 +15,6 @@ return [
     'failed'   => '用户名或密码错误。',
     'password' => '密码错误。',
     'throttle' => '您尝试的登录次数过多，请 :seconds 秒后再试。',
+    'updateSuccessfully' => '更新成功',
+    'Deleted successfully' => '删除成功'
 ];
