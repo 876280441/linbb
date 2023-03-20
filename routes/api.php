f1049fa0c30b6b291987739699f924c25284f17e
linruxin <876280441@qq.com>
2023-03-20 23:04:22 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->name('api.v1.')->group(function() {
-    // 短信验证码
-    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', [UsersController::class, 'store'])->name('users.store');
+    // 登录相关
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))
+        ->group(function () {
+            // 短信验证码
+            Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
+            // 用户注册
+            Route::post('users', [UsersController::class, 'store'])->name('users.store');
+        });
+
+    // 访问频率限制
+    Route::middleware('throttle:' . config('api.rate_limits.access'))
+        ->group(function () {
+
+        });
+
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
